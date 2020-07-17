7bd221a02679c75c1747d9a43aa5c35db9b73747
veass <veass@users.noreply.github.com>
2020-07-17 06:01:56 +0300

    new

---
+++ dist/js/scripts.js
@@ -0,0 +1,98 @@
+$('.switch-button').click(function(){
+  if ($(this).parent().parent().find('.collapse').hasClass('show') == false){
+    $($(this).parent().parent()).css({
+     'border-bottom':'1px solid #e5e5e5'
+    });
+    $($(this).parent()).css({
+     'border':'0'
+    });
+    $(this).find('.rotate').css({ 
+            'transition': 'transform 0.5s',
+            '-o-transform': 'rotate(180deg)',
+            '-ms-transform': 'rotate(180deg)',
+            '-moz-transform': 'rotate(180deg)',
+            '-webkit-transform': 'rotate(180deg)',
+            'transform': 'rotate(180deg)'
+    });
+  }
+  else {
+    $(this).find('.triple').css({
+      'border-bottom': '1px solid #e5e5e5 '
+    });
+    $(this).find('.rotate').css({
+      'transition': 'transform 0.5s',
+      '-o-transform': 'rotate(0deg)',
+      '-ms-transform': 'rotate(0deg)',
+      '-moz-transform': 'rotate(0deg)',
+      '-webkit-transform': 'rotate(0deg)',
+      'transform': 'rotate(0deg)'
+    })
+  }
+  })
+
+ 
+  // Regroup our blocks\
+let header = $('header');
+let news = $('.button-news');
+let contentBlock = $('.content-block');
+
+function resizeForheader(){
+  // перемещение хэдэра
+  header.detach();
+  contentBlock.prepend(header);
+  //адаптация элементов под мобильное устройство
+  header.addClass('mobile-header');
+  $('.header-content').removeClass('flex-column');
+  $('.ham').css({'display':'block'});
+  news.detach()
+  $('.news-active__bottom').append(news);
+  news.css({
+    'text-align': 'center',
+    'padding': '16px 0',
+    'border': '1px solid',
+    'margin-top': '50px'
+  })
+  news.find('span').html('Все новости')
+                   .css({'font-size': '14px'})
+  $('.news-active__bottom').removeClass('flex-row');
+  $('.news-active__bottom').addClass('flex-column');
+  $('.new-item svg').detach();    
+}
+function getBacktoheader(){
+  $('.container-xxl').prepend(header)
+  header.removeClass('mobile-header')
+  header.find('.ham').css({
+    'display':'none'
+  })
+  $('.header-content').addClass('flex-column');
+  $('.news-active__bottom').removeClass('flex-column');
+  $('.news-active__bottom').addClass('flex-row');
+  $('.news-active__top').append(news);
+  news.css({
+    'border-top': '1px solid',
+    'margin-top': '0px'
+  })
+
+}
+
+  if(($(window).width() >= 360 && $(window).width() <= 1100)){
+    resizeForheader();
+    
+  }
+
+  $(window).resize(function(){
+    if($(window).width() >= 360 && $(window).width()<= 1100){
+      resizeForheader();
+
+    }
+    if($(window).width() > 1100){
+      getBacktoheader();
+    }
+  })
+
+//menu 
+
+    $('.ham').click(function(){
+      $('.nav > ul').toggleClass('menu-open');
+    })
+
